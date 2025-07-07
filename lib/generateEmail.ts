09ad369cdf1ca4c7dfc9ed317a07b65ae4f295db
PancaGamaStore <108522948+PancaGamaStore@users.noreply.github.com>
2025-07-08 04:31:50 +0700

    Update generateEmail.ts

--- lib/generateEmail.ts
+++ lib/generateEmail.ts
@@ -1,4 +1,12 @@
+const domains = ["okekang.my.id", "jaycok.my.id"];
+
 export function generateRandomUsername() {
   const chars = "abcdefghijklmnopqrstuvwxyz0123456789";
   return Array.from({ length: 8 }, () => chars[Math.floor(Math.random() * chars.length)]).join("");
+}
+
+export function generateRandomEmail() {
+  const user = generateRandomUsername();
+  const domain = domains[Math.floor(Math.random() * domains.length)];
+  return `${user}@${domain}`;
 }
